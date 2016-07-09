f92496ca411073483eb9b833371c3d8e54b25247
Bradley Bain <brad@bradleys-mbp.attlocal.net>
2016-07-09 17:20:44 -0500

    Add page transitions

--- js/app.js
+++ js/app.js
@@ -4,24 +4,33 @@ $(function(){
     prefetch: true,
     cacheLength: 2,
     onStart: {
-      duration: 250, // Duration of our animation
+      duration: 300, // Duration of our animation
       render: function ($container) {
         // Add your CSS animation reversing class
-        $container.addClass('is-exiting');
+        $container.addClass('animated fadeOut');
 
         // Restart your animation
         smoothState.restartCSSAnimations();
       }
     },
     onReady: {
-      duration: 0,
+      duration: 300,
       render: function ($container, $newContent) {
         // Remove your CSS animation reversing class
-        $container.removeClass('is-exiting');
+        $container.removeClass('animated fadeOut');
 
         // Inject the new content
+        $container.hide();
         $container.html($newContent);
+        $container.show();
 
+        $container.addClass('animated fadeIn');
+      }
+    },
+    onAfter: {
+      render: function ($container) {
+        // Remove your CSS animation intro class
+        $container.removeClass('animated fadeIn');
       }
     }
   },
