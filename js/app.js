ba53f64511b09138b841df37f94897e716a09034
Bradley Bain <brad@bradleys-mbp.attlocal.net>
2016-07-09 16:48:51 -0500

    Add smoothstate.js

--- js/app.js
+++ js/app.js
@@ -1,43 +1,29 @@
-var pageLoader = {
-    init: function() {
-        this.events();
-    },
-    events: function() {
-        var that = this;
-        $("#about").click(function() {
-            that.loadPage("about.html");
-        });
-    },
-    loadPage: function(page) {
-        var that = this;
-        $.get("pages/" + page, function(content) {
-            var panel = $(".panel");
-            panel.html(content);
-            that.openPanel();
-        });
-    },
-    openPanel: function(specifier) {
-        $(".panel").show();
+$(function(){
+  'use strict';
+  var options = {
+    prefetch: true,
+    cacheLength: 2,
+    onStart: {
+      duration: 250, // Duration of our animation
+      render: function ($container) {
+        // Add your CSS animation reversing class
+        $container.addClass('is-exiting');
 
-        var gridSizer = new GridSizer(".panel", ($(".panel").height() - $(".content h2").height()) * 0.7);
-        gridSizer.init();
-
-        //automatic scroll - http://stackoverflow.com/a/6677069
-        $('html, body').animate({
-            scrollTop: $(".panel").offset().top
-        }, 500);
+        // Restart your animation
+        smoothState.restartCSSAnimations();
+      }
     },
-    closePanels: function() {
-        $(".panel").hide();
+    onReady: {
+      duration: 0,
+      render: function ($container, $newContent) {
+        // Remove your CSS animation reversing class
+        $container.removeClass('is-exiting');
+
+        // Inject the new content
+        $container.html($newContent);
+
+      }
     }
-};
-
-$(document).ready(function() {
-    pageLoader.init();
-
-    /*$('body').flowtype({
-        minFont: 14,
-        maxFont: 30,
-        fontRatio: 35
-    });*/
+  },
+  smoothState = $('#content').smoothState(options).data('smoothState');
 });
